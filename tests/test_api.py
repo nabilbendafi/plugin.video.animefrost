30b46f16530bf6be4163d93a39f7252132187640
Nabil Bendafi <nabil@bendafi.fr>
2015-12-09 11:33:03 +0100

    Tests introduction

---
+++ tests/test_api.py
@@ -0,0 +1,42 @@
+import unittest
+
+import animefrost
+
+class AnimeFrostTestCase(unittest.TestCase):
+
+    def setUp(self):
+        api = animefrost.API()
+
+    def test_instance(self):
+        pass
+
+    def test_browser(self):
+        pass
+
+    def test_all(self):
+        pass
+
+    def test_categories(self):
+        pass
+
+    def test_category(self):
+        pass
+
+    def test_animes(self):
+        pass
+
+    def test_anime(self):
+        pass
+
+    def test_episode(self):
+        pass
+
+    def test_search(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    #Rock'n'Roll
+    unittest.main()
